49d2c0e843012d100d446505d8b4b5f099d32f8f
Simon Mika <simonmika@users.noreply.github.com>
2020-03-24 09:28:16 +0100

    Added ability to schedule tasks in background and finish them. (#16)
    * Added ability to schedule tasks in background and finish them.
    
    * Added precaution in case of reuse.

--- index.ts
+++ index.ts
@@ -3,9 +3,21 @@ import { Request } from "./Request"
 import { Response } from "./Response"
 import { create } from "./Response/create"
 
+let tasks: Promise<void>[] = []
+async function finish(): Promise<void> {
+	const t = tasks
+	tasks = []
+	await Promise.all(t)
+}
+function schedule(task: Promise<void>): void {
+	tasks.push(task)
+}
+
 export {
 	Endpoint,
 	Request,
 	Response,
 	create,
+	finish,
+	schedule,
 }
