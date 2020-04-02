99fa28db7f9e8630258227f708d1b3e5263db79a
Simon Mika <simonmika@users.noreply.github.com>
2020-04-02 14:02:17 +0200

    Added try-catch around endpoints. (#18)

--- index.ts
+++ index.ts
@@ -2,22 +2,12 @@ import { Endpoint } from "./Endpoint"
 import { Request } from "./Request"
 import { Response } from "./Response"
 import { create } from "./Response/create"
-
-let tasks: Promise<void>[] = []
-async function finish(): Promise<void> {
-	const t = tasks
-	tasks = []
-	await Promise.all(t)
-}
-function schedule(task: Promise<void>): void {
-	tasks.push(task)
-}
+import { schedule } from "./schedule"
 
 export {
 	Endpoint,
 	Request,
 	Response,
 	create,
-	finish,
 	schedule,
 }
