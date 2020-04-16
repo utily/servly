792a99444f2098c6dab279abd7173ca1861448fa
Simon Mika <simon@mika.se>
2020-04-16 16:50:52 +0200

    Added support for timer functions.

---
+++ Timer.ts
@@ -0,0 +1,16 @@
+import { finish } from "./schedule"
+export type Timer = () => Promise<void>
+
+export namespace Timer {
+	export function create(handler: () => Promise<void>): Timer {
+		return async () => {
+			try {
+				await handler()
+			} catch (error) {
+				console.log("servly timer catch", error)
+			}
+			await finish()
+		}
+	}
+	export type Ejector = (endpoint: Timer) => void
+}
