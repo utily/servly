da3d13e09d68f54f553b8bee051363ed5e4c5b77
Simon Mika <simon@mika.se>
2020-04-21 15:37:36 +0200

    Moved context argument first.

--- Endpoint.ts
+++ Endpoint.ts
@@ -4,16 +4,16 @@ import { Response } from "./Response"
 import { Request } from "./Request"
 import { finish } from "./schedule"
 
-export type Endpoint = (request: Omit<Partial<Request>, "body"> & { body?: Promise<any> | object | any }, context?: Partial<Context>) => Promise<Required<Response>>
+export type Endpoint = (context: Partial<Context>, request: Omit<Partial<Request>, "body"> & { body?: Promise<any> | object | any }) => Promise<Required<Response>>
 
 export namespace Endpoint {
-	export function create(endpoint: ((request: Request, context: Context) => Promise<Response | any>) | ((request: Request) => Promise<Response | any>)): Endpoint {
-		return async (request, context) => {
+	export function create(endpoint: (context: Context, request: Request) => Promise<Response | any>): Endpoint {
+		return async (context, request) => {
 			const c = Context.create(context)
 			const input = Request.create(request)
 			let output: Response | any
 			try {
-				output = await endpoint(input, c)
+				output = await endpoint(c, input)
 			} catch (error) {
 				c.log("servly.catch", "error", error)
 				output = gracely.server.unknown()
--- Timer.ts
+++ Timer.ts
@@ -4,7 +4,7 @@ import { finish } from "./schedule"
 export type Timer = (context?: Partial<Context>) => Promise<void>
 
 export namespace Timer {
-	export function create(handler: ((context: Context) => Promise<void>) | (() => Promise<void>)): Timer {
+	export function create(handler: (context: Context) => Promise<void>): Timer {
 		return async (context: Partial<Context>) => {
 			const c = Context.create(context)
 			try {
