d67c92ad1c032eedd945a72efb0aefb50b69dd4a
Robin Nedergård <robin.nedergard@payfunc.com>
2020-09-29 08:41:01 +0200

    corrected test response

--- Response/create.spec.ts
+++ Response/create.spec.ts
@@ -2,5 +2,5 @@ import * as servly from "../index"
 
 describe("servly.Response.create", () => {
 	it("array", async () => expect(servly.Response.create([]).header.contentType).toEqual("application/json; charset=utf-8"))
-	it("array return", async () => expect(servly.Response.create([])).toEqual([]))
+	it("array return", async () => expect(servly.Response.create([])).toEqual({"body": [], "header": {"contentType": "application/json; charset=utf-8"}, "status": 200}))
 })
