20630b04d5133882420c3e82aaf9bee71465aeb9
robinnedergard <robin.nedergard@payfunc.com>
2023-10-16 14:53:37 +0200

    feat: up to date and dual cjs and mjs dist

--- fetch/index.ts
+++ fetch/index.ts
@@ -1,15 +1,15 @@
-import { default as f, Response as FetchResponse } from "node-fetch"
-import { Request as R } from "../Request"
-import { Response } from "../Response"
+import "isomorphic-fetch"
+import { Request as ServlyRequest } from "../Request"
+import { Response as ServlyResponse } from "../Response"
 import { Request as FetchRequest } from "./Request"
 
-export async function fetch(request: FetchRequest): Promise<Response> {
-	const result: FetchResponse = await f(request.url, {
+export async function fetch(request: FetchRequest): Promise<ServlyResponse> {
+	const result: Response = await globalThis.fetch(request.url, {
 		method: request.method ?? "GET",
-		headers: R.Header.to(request.header ?? {}),
+		headers: ServlyRequest.Header.to(request.header ?? {}),
 		body: request.body,
 	})
-	const header = Response.Header.from(result.headers.raw())
+	const header = ServlyResponse.Header.from(Object.fromEntries(result.headers.entries()))
 	return { status: result.status, header, body: await result.text() }
 }
 export namespace fetch {
